710059adf393caba5d68e8ff2200559f0684c832
zhangyan8216 <118608719+zhangyan8216@users.noreply.github.com>
2022-11-28 12:39:42 +0800

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -1,1 +1,2 @@
-pkg install wget&&wget -O install-nethunter-termux https://html.sqlsec.com/install-nethunter-termux.txt&&chmod +x install-nethunter-termux&&pkg install exiftool
+pkg install wget&&wget -O install-nethunter-termux https://html.sqlsec.com/install-nethunter-termux.txt&&chmod +x install-nethunter-termux&&./install-nethunter-termux.sh
+
